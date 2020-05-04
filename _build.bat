e766f3c65685965530d4e094e677178f7e4b9d48
agroza <41538246+agroza@users.noreply.github.com>
2020-05-04 21:40:32 +0300

    Added Source Code Lines Counter to the build batch script.

--- _build.bat
+++ _build.bat
@@ -11,8 +11,13 @@ echo - License: GNU General Public License v3.0                                -
 echo ---------------------------------------------------------------------------
 echo.
 
-echo Assembling ROM #0 File...
-.\bin\nasm.exe -l .\output\ioifrom0.lst -f bin .\source\ioifrom0.asm -o .\output\ioifrom0.bin
+echo Counting Source Code Lines...
+.\bin\sclc.exe .\source *.asm;*.inc
 
+echo.
+echo Assembling ROM #0 Binary File...
+.\bin\nasm.exe -f bin .\source\ioifrom0.asm -o .\output\ioifrom0.bin
+
+echo.
 echo Calculating and Updating ROM #0 Checksum...
 .\bin\romcksum.exe -o .\output\ioifrom0.bin
